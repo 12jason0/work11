55a76e502d65f8263adb723b4b66b13277765872
jason <154851741+12jason0@users.noreply.github.com>
2024-01-10 19:00:30 +0900

    Add files via upload

---
+++ 0110/exam.js
@@ -0,0 +1,20 @@
+function apple() {
+    $('.img').attr('src', '0110apple.jpg');
+    $('b').remove();
+    $('.img').css({ width: '200px', hight: '200px' });
+}
+function banana() {
+    $('.img').attr('src', '0110banana.jpg');
+    $('b').remove();
+    $('.img').css({ width: '200px', hight: '200px' });
+}
+function grape() {
+    $('.img').attr('src', '0110grape.jpg');
+    $('b').remove();
+    $('.img').css({ width: '200px', hight: '200px' });
+}
+function peach() {
+    $('.img').attr('src', '0110peach.jpg');
+    $('b').remove();
+    $('.img').css({ width: '200px', hight: '200px' });
+}
