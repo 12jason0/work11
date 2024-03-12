379833e96e1ce920e02e25e57029010fe30ffab7
12jason0 <12jason0@spreatics.com>
2024-03-12 13:49:53 +0900

    react-props1

---
+++ 3mon/0312/0312-props/src/FunctionComponent.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export default function FunctionComponent({ name, time, children }) {
+  //구조 분해 할당을 FunctionComponent(props) <= 여기서도 가능함 ex) {name, time}
+  //   const { name, time } = props;
+  return (
+    <div>
+      <h1>Hi {name}</h1>
+      {/* <h1>Hi {props.name}</h1> */}
+      {/* <h1>Hi {props.time}</h1> */}
+      <h1>Hi {time}</h1>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+FunctionComponent.defaultProps = {
+  //기본값으로 설정 가능
+  name: '홍길동',
+};
+
+FunctionComponent.propTypes = {
+  name: PropTypes.string,
+};
